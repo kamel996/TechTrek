95d310d10ec9321942679a60dd38d26cdeb97bad
kamel996 <kamelnassar2828@gmail.com>
2023-04-17 14:34:34 +0300

    adding some error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productsRoutes.js";
-
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 // My server
 
 dotenv.config();
@@ -16,5 +16,9 @@ app.get("/", (req, res) => {
   res.send("Api is running");
 });
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(5000, console.log(`Server running on port ${PORT}`));
