e129b75004142551b0c978dbb8b159d0a01117fd
kamel996 <kamelnassar2828@gmail.com>
2023-04-22 20:43:36 +0300

    first step user auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,15 +3,20 @@ import asyncHandler from "express-async-handler";
 
 export const authUser = asyncHandler(async (req, res) => {
  
-  const user = await User.findOne({ email :req.body.email});
+  const user = await User.findOne({ email:req.body.email});
   console.log(user,"jihuhuuh")
+  console.log("email:", req.body.email);
+console.log(req.body);
+
   
 
   if (user && (await user.matchPassword(req.body.password))) {
     res.json({
       _id: user._id,
+      name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+      token: null
     }
     );
   } else {
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productsRoutes.js";
 import userRoutes from "./routes/userRoute.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import bodyParser from "body-parser";
+
 import cors from "cors";
 
 // My server
@@ -13,8 +15,11 @@ cors();
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
+
+app.use(express.json());
 
 const PORT = process.env.PORT;
 
